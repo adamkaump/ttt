f69c4ee15c320716f928b1b0b6aeb8d831a34465
Adam Kaump <adam.kaump@gmail.com>
2018-05-09 15:31:12 -0400

    Eliminating variables

--- Sources/Run/CommonApi.swift
+++ Sources/Run/CommonApi.swift
@@ -17,68 +17,68 @@ let pemPath = "App.framework/Versions/A/Resources/eckey.pem"
 
 class CommonAPI {
     
-    class func ckRequest(base: String, subpath: String, data: [String: AnyObject]) -> URLRequest {
-        let urlString = "\(base)\(subpath)"
-        let url = URL(string: urlString)!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-//        let auth = PrivateKeyAuthenticator(apiKeyID: apiKeyID, pathToPEM: pemPath)
-//        let bodyData = try! JSONSerialization.data(withJSONObject: data)
-//        let additionalHeaders = auth.signedHeaders(for: bodyData, query: subpath)
-//        for (name, value) in additionalHeaders {
-//            request.addValue(value, forHTTPHeaderField: name)
-//        }
-//        request.httpBody = bodyData
-        return request
-    }
+//    class func ckRequest(base: String, subpath: String, data: [String: AnyObject]) -> URLRequest {
+//        let urlString = "\(base)\(subpath)"
+//        let url = URL(string: urlString)!
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+////        let auth = PrivateKeyAuthenticator(apiKeyID: apiKeyID, pathToPEM: pemPath)
+////        let bodyData = try! JSONSerialization.data(withJSONObject: data)
+////        let additionalHeaders = auth.signedHeaders(for: bodyData, query: subpath)
+////        for (name, value) in additionalHeaders {
+////            request.addValue(value, forHTTPHeaderField: name)
+////        }
+////        request.httpBody = bodyData
+//        return request
+//    }
 
-    class func fetchUser(userId: String, completion: @escaping ([String: AnyObject]?) -> Void) {
-//    class func fetchUser(bet: [String: AnyObject], completion: @escaping ([String: AnyObject]?) -> Void) {
-        
-        print("fetching user: \(userId)")
-        
-        let request = self.ckRequest(base: baseUrlString, subpath: fetchSubpath, data: self.userQueryDictionary(userId: userId))
-        let session = URLSession(configuration: .default)
-        session.dataTask(with: request) { (data, response, error) in
-            
-            guard data != nil, data!.toJson()!["records"] != nil else {
-                completion(nil)
-                return
-            }
-            
-            let records = data!.toJson()!["records"] as! [[String: AnyObject]]
-            let user = records.first!
-            completion(user)
-            }.resume()
-    }
-    
+//    class func fetchUser(userId: String, completion: @escaping ([String: AnyObject]?) -> Void) {
+////    class func fetchUser(bet: [String: AnyObject], completion: @escaping ([String: AnyObject]?) -> Void) {
+//
+//        print("fetching user: \(userId)")
+//
+//        let request = self.ckRequest(base: baseUrlString, subpath: fetchSubpath, data: self.userQueryDictionary(userId: userId))
+//        let session = URLSession(configuration: .default)
+//        session.dataTask(with: request) { (data, response, error) in
+//
+//            guard data != nil, data!.toJson()!["records"] != nil else {
+//                completion(nil)
+//                return
+//            }
+//
+//            let records = data!.toJson()!["records"] as! [[String: AnyObject]]
+//            let user = records.first!
+//            completion(user)
+//            }.resume()
+//    }
+//
 }
 
-extension CommonAPI {
-    class func userQueryDictionary(userId: String) -> [String: AnyObject] {
-        var dict = [String: AnyObject]()
-        dict["records"] = ["recordName": userId] as AnyObject
-        return dict
-    }
-    
-    class func userBankUpdateDictionary(user: [String: AnyObject], newBank: Int) -> [String: AnyObject] {
-        let recordName = user["recordName"] as! String
-        let changeTag = user["recordChangeTag"] as! String
-        
-        var dict = [String: AnyObject]()
-        dict["operations"] = ["operationType": "update", "record": ["recordName": recordName, "recordChangeTag": changeTag, "fields": ["bank": ["value": newBank]]]] as AnyObject
-        return dict
-    }
-}
+//extension CommonAPI {
+//    class func userQueryDictionary(userId: String) -> [String: AnyObject] {
+//        var dict = [String: AnyObject]()
+//        dict["records"] = ["recordName": userId] as AnyObject
+//        return dict
+//    }
+//
+//    class func userBankUpdateDictionary(user: [String: AnyObject], newBank: Int) -> [String: AnyObject] {
+//        let recordName = user["recordName"] as! String
+//        let changeTag = user["recordChangeTag"] as! String
+//
+//        var dict = [String: AnyObject]()
+//        dict["operations"] = ["operationType": "update", "record": ["recordName": recordName, "recordChangeTag": changeTag, "fields": ["bank": ["value": newBank]]]] as AnyObject
+//        return dict
+//    }
+//}
 
-extension Dictionary where Key == String, Value == AnyObject {
-    func fieldValueFor(_ key: String) -> AnyObject {
-        let fields = self["fields"] as AnyObject
-        let keyDict = fields[key] as AnyObject
-        let value = keyDict["value"] as AnyObject
-        return value
-    }
-}
+//extension Dictionary where Key == String, Value == AnyObject {
+//    func fieldValueFor(_ key: String) -> AnyObject {
+//        let fields = self["fields"] as AnyObject
+//        let keyDict = fields[key] as AnyObject
+//        let value = keyDict["value"] as AnyObject
+//        return value
+//    }
+//}
 
 extension Data {
     func toJson() -> [String: Any]? {
